973c2c510e5f04fc9a72d84e5c100170112f3046
Matthew Rodusek <matthew.rodusek@gmail.com>
2016-12-13 23:47:41 -0500

    Added bit::string_tokenizer -> bit::stl::string_tokenizer
    Updated string_tokenizer tests

--- detail/string_tokenizer.inl
+++ detail/string_tokenizer.inl
@@ -2,144 +2,145 @@
 #define BFSTL_DETAIL_STRING_TOKENIZER_INL
 
 namespace bit {
+  namespace stl {
 
-  //--------------------------------------------------------------------------
-  // Constructor
-  //--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+    // Constructor
+    //--------------------------------------------------------------------------
 
-  template<typename CharT, typename Traits>
-  inline constexpr basic_string_tokenizer<CharT,Traits>::
-    basic_string_tokenizer( string_type str,
-                            string_type delim,
-                            bool return_delims )
-    : m_buffer(std::move(str)),
-      m_delimiter(std::move(delim)),
-      m_current(0),
-      m_return_delims(return_delims)
-  {
-
-  }
-
-  //-------------------------------------------------------------------------
-  // Accessors
-  //-------------------------------------------------------------------------
-
-  template<typename CharT, typename Traits>
-  inline constexpr typename basic_string_tokenizer<CharT,Traits>::string_type
-    basic_string_tokenizer<CharT,Traits>::delimiters()
-    const noexcept
-  {
-    return m_delimiter;
-  }
-
-  template<typename CharT, typename Traits>
-  inline constexpr typename basic_string_tokenizer<CharT,Traits>::string_type
-    basic_string_tokenizer<CharT,Traits>::buffer()
-    const noexcept
-  {
-    return m_buffer;
-  }
-
-  template<typename CharT, typename Traits>
-  inline constexpr bool basic_string_tokenizer<CharT,Traits>::keeps_delimiter()
-    const noexcept
-  {
-    return m_return_delims;
-  }
-
-  //--------------------------------------------------------------------------
-  // Tokenizing
-  //--------------------------------------------------------------------------
-
-  template<typename CharT, typename Traits>
-  inline constexpr bool
-    basic_string_tokenizer<CharT,Traits>::has_more()
-    const noexcept
-  {
-    return m_current != m_buffer.size();
-  }
-
-  template<typename CharT, typename Traits>
-  inline typename basic_string_tokenizer<CharT,Traits>::string_type
-    basic_string_tokenizer<CharT,Traits>::next()
-    noexcept
-  {
-    return next_token( m_delimiter );
-  }
-
-  template<typename CharT, typename Traits>
-  inline typename basic_string_tokenizer<CharT,Traits>::string_type
-    basic_string_tokenizer<CharT,Traits>::next( const string_type& delim )
-    noexcept
-  {
-    return next_token( delim );
-  }
-
-  template<typename CharT, typename Traits>
-  inline void
-    basic_string_tokenizer<CharT,Traits>::reset()
-    noexcept
-  {
-    m_current = 0;
-  }
-
-  //-------------------------------------------------------------------------
-  // Private Member Functions
-  //-------------------------------------------------------------------------
-
-  template<typename CharT, typename Traits>
-  inline void basic_string_tokenizer<CharT,Traits>::
-    skip_delimiter()
-    noexcept
-  {
-    if(!m_return_delims && (m_current < m_buffer.size())) ++m_current;
-  }
-
-  //-------------------------------------------------------------------------
-
-  template<typename CharT, typename Traits>
-  inline typename basic_string_tokenizer<CharT,Traits>::string_type
-    basic_string_tokenizer<CharT,Traits>::
-    next_token( const string_type& delim )
-    noexcept
-  {
-    if(!has_more()) return "";
-
-    auto delim_found = false;
-    auto start       = m_current;
-
-    while( m_current < m_buffer.size() && !delim_found )
+    template<typename CharT, typename Traits>
+    inline constexpr basic_string_tokenizer<CharT,Traits>::
+      basic_string_tokenizer( string_type str,
+                              string_type delim,
+                              bool return_delims )
+      : m_buffer(std::move(str)),
+        m_delimiter(std::move(delim)),
+        m_current(0),
+        m_return_delims(return_delims)
     {
-      auto character = m_buffer[m_current];
-      if( is_delimiter( character, delim ) )
+
+    }
+
+    //-------------------------------------------------------------------------
+    // Accessors
+    //-------------------------------------------------------------------------
+
+    template<typename CharT, typename Traits>
+    inline constexpr typename basic_string_tokenizer<CharT,Traits>::string_type
+      basic_string_tokenizer<CharT,Traits>::delimiters()
+      const noexcept
+    {
+      return m_delimiter;
+    }
+
+    template<typename CharT, typename Traits>
+    inline constexpr typename basic_string_tokenizer<CharT,Traits>::string_type
+      basic_string_tokenizer<CharT,Traits>::buffer()
+      const noexcept
+    {
+      return m_buffer;
+    }
+
+    template<typename CharT, typename Traits>
+    inline constexpr bool basic_string_tokenizer<CharT,Traits>::keeps_delimiter()
+      const noexcept
+    {
+      return m_return_delims;
+    }
+
+    //--------------------------------------------------------------------------
+    // Tokenizing
+    //--------------------------------------------------------------------------
+
+    template<typename CharT, typename Traits>
+    inline constexpr bool
+      basic_string_tokenizer<CharT,Traits>::has_more()
+      const noexcept
+    {
+      return m_current != m_buffer.size();
+    }
+
+    template<typename CharT, typename Traits>
+    inline typename basic_string_tokenizer<CharT,Traits>::string_type
+      basic_string_tokenizer<CharT,Traits>::next()
+      noexcept
+    {
+      return next_token( m_delimiter );
+    }
+
+    template<typename CharT, typename Traits>
+    inline typename basic_string_tokenizer<CharT,Traits>::string_type
+      basic_string_tokenizer<CharT,Traits>::next( const string_type& delim )
+      noexcept
+    {
+      return next_token( delim );
+    }
+
+    template<typename CharT, typename Traits>
+    inline void
+      basic_string_tokenizer<CharT,Traits>::reset()
+      noexcept
+    {
+      m_current = 0;
+    }
+
+    //-------------------------------------------------------------------------
+    // Private Member Functions
+    //-------------------------------------------------------------------------
+
+    template<typename CharT, typename Traits>
+    inline void basic_string_tokenizer<CharT,Traits>::
+      skip_delimiter()
+      noexcept
+    {
+      if(!m_return_delims && (m_current < m_buffer.size())) ++m_current;
+    }
+
+    //-------------------------------------------------------------------------
+
+    template<typename CharT, typename Traits>
+    inline typename basic_string_tokenizer<CharT,Traits>::string_type
+      basic_string_tokenizer<CharT,Traits>::
+      next_token( const string_type& delim )
+      noexcept
+    {
+      if(!has_more()) return "";
+
+      auto delim_found = false;
+      auto start       = m_current;
+
+      while( m_current < m_buffer.size() && !delim_found )
       {
-        delim_found = true;
-      } else {
-        ++m_current;
+        auto character = m_buffer[m_current];
+        if( is_delimiter( character, delim ) )
+        {
+          delim_found = true;
+        } else {
+          ++m_current;
+        }
       }
+      if(m_current==start && m_return_delims) ++m_current;
+
+      auto end = m_current - start;
+      skip_delimiter();
+
+      return m_buffer.substr( start, end );
     }
-    if(m_current==start && m_return_delims) ++m_current;
 
-    auto end = m_current - start;
-    skip_delimiter();
+    //-------------------------------------------------------------------------
 
-    return m_buffer.substr( start, end );
-  }
-
-  //-------------------------------------------------------------------------
-
-  template<typename CharT, typename Traits>
-  inline bool basic_string_tokenizer<CharT,Traits>::
-    is_delimiter( value_type c, const string_type& delim )
-    const noexcept
-  {
-    for( const value_type& d : delim )
+    template<typename CharT, typename Traits>
+    inline bool basic_string_tokenizer<CharT,Traits>::
+      is_delimiter( value_type c, const string_type& delim )
+      const noexcept
     {
-      if( d == c ) return true;
+      for( const value_type& d : delim )
+      {
+        if( d == c ) return true;
+      }
+      return false;
     }
-    return false;
-  }
-
+  } // namespace stl
 } // namespace bit
 
 #endif /* BFSTL_DETAIL_STRING_TOKENIZER_INL */
--- string_tokenizer.hpp
+++ string_tokenizer.hpp
@@ -12,148 +12,150 @@
 #include "string_view.hpp"
 
 namespace bit {
+  namespace stl {
 
-  /////////////////////////////////////////////////////////////////////////////
-  /// \class bit::basic_string_tokenizer
-  ///
-  /// \brief A tokenizer class for strings.
-  ///
-  /// This class performs simple tokenization of strings in a lightweight
-  /// manner, by making heavy-use of basic_string_view types.
-  ///
-  /////////////////////////////////////////////////////////////////////////////
-  template<typename CharT, typename Traits = std::char_traits<CharT>>
-  class basic_string_tokenizer final
-  {
-    //-------------------------------------------------------------------------
-    // Public Types
-    //-------------------------------------------------------------------------
-  public:
-
-    using value_type  = CharT;
-    using traits_type = Traits;
-    using string_type = basic_string_view<CharT,Traits>;
-
-    using size_type = typename basic_string_view<CharT,Traits>::size_type;
-
-    //-------------------------------------------------------------------------
-    // Construction
-    //-------------------------------------------------------------------------
-  public:
-
-    /// \brief Tokenizes a given string with the specified delimiter, with a
-    ///        given option to return delimiters are part of the token
+    /////////////////////////////////////////////////////////////////////////////
+    /// \class bit::basic_string_tokenizer
     ///
-    /// \param str the string to tokenize
-    /// \param delim the delimiter to use for tokenization
-    /// \param return_delims Flag indicating whether to return delimiters
-    constexpr explicit basic_string_tokenizer( string_type str,
-                                               string_type delim,
-                                               bool return_delims = false );
-
-    /// \brief Copies a string tokenizer
+    /// \brief A tokenizer class for strings.
     ///
-    /// \param other the other tokenizer to copy
-    constexpr basic_string_tokenizer( const basic_string_tokenizer& other ) = default;
-
-    /// \brief Moves a string tokenizer
+    /// This class performs simple tokenization of strings in a lightweight
+    /// manner, by making heavy-use of basic_string_view types.
     ///
-    /// \param other the other tokenizer to move
-    constexpr basic_string_tokenizer( basic_string_tokenizer&& other ) = default;
+    /////////////////////////////////////////////////////////////////////////////
+    template<typename CharT, typename Traits = std::char_traits<CharT>>
+    class basic_string_tokenizer final
+    {
+      //-------------------------------------------------------------------------
+      // Public Types
+      //-------------------------------------------------------------------------
+    public:
 
-    //-------------------------------------------------------------------------
-    // Accessors
-    //-------------------------------------------------------------------------
-  public:
+      using value_type  = CharT;
+      using traits_type = Traits;
+      using string_type = basic_string_view<CharT,Traits>;
 
-    /// \brief Retrieves the delimiters for this tokenizer
-    ///
-    /// \return the delimiters
-    constexpr string_type delimiters() const noexcept;
+      using size_type = typename basic_string_view<CharT,Traits>::size_type;
 
-    /// \brief Retrieves the string used in this tokenizer
-    ///
-    /// \return the string
-    constexpr string_type buffer() const noexcept;
+      //-------------------------------------------------------------------------
+      // Construction
+      //-------------------------------------------------------------------------
+    public:
 
-    /// \brief Returns whether this tokenizer includes the delimiters
-    ///
-    /// \return \c true if this tokenizer returns the delimiters
-    constexpr bool keeps_delimiter() const noexcept;
+      /// \brief Tokenizes a given string with the specified delimiter, with a
+      ///        given option to return delimiters are part of the token
+      ///
+      /// \param str the string to tokenize
+      /// \param delim the delimiter to use for tokenization
+      /// \param return_delims Flag indicating whether to return delimiters
+      constexpr explicit basic_string_tokenizer( string_type str,
+                                                 string_type delim,
+                                                 bool return_delims = false );
 
-    //-------------------------------------------------------------------------
-    // Tokenizing
-    //-------------------------------------------------------------------------
-  public:
+      /// \brief Copies a string tokenizer
+      ///
+      /// \param other the other tokenizer to copy
+      constexpr basic_string_tokenizer( const basic_string_tokenizer& other ) = default;
 
-    /// \brief Returns true if the tokenizer has more tokens available
-    ///
-    /// \return true if the tokenizer has more tokens
-    constexpr bool has_more() const noexcept;
+      /// \brief Moves a string tokenizer
+      ///
+      /// \param other the other tokenizer to move
+      constexpr basic_string_tokenizer( basic_string_tokenizer&& other ) = default;
 
-    /// \brief Gets the next token in this string tokenizer's string.
-    ///
-    /// \return the next token
-    string_type next() noexcept;
+      //-------------------------------------------------------------------------
+      // Accessors
+      //-------------------------------------------------------------------------
+    public:
 
-    /// \brief Gets the next token in this string tokenizer's string.
-    ///
-    /// First, the set of characters considered to be delimiters by this
-    /// basic_string_tokenizer object is changed to be the characters in
-    /// the string \p delim.
-    ///
-    /// Then the next token in the string after the current position is
-    /// returned. The current position is advanced beyond the recognized
-    /// token. The new delimiter set remains the default after this call
-    ///
-    /// \param delim the delimiter to match
-    /// \return the next token
-    string_type next( const string_type& delim ) noexcept;
+      /// \brief Retrieves the delimiters for this tokenizer
+      ///
+      /// \return the delimiters
+      constexpr string_type delimiters() const noexcept;
 
-    /// \brief Resets the current position within this string tokenizer
-    ///        to the beginning of the buffer
-    ///
-    /// This call allows for the basic_string_tokenizer to be re-used as if
-    /// reconstructed.
-    void reset() noexcept;
+      /// \brief Retrieves the string used in this tokenizer
+      ///
+      /// \return the string
+      constexpr string_type buffer() const noexcept;
 
-    //-------------------------------------------------------------------------
-    // Private Member Functions
-    //-------------------------------------------------------------------------
-  private:
+      /// \brief Returns whether this tokenizer includes the delimiters
+      ///
+      /// \return \c true if this tokenizer returns the delimiters
+      constexpr bool keeps_delimiter() const noexcept;
 
-    /// \brief Skips the delimiter, if \c return_delims was false
-    void skip_delimiter() noexcept;
+      //-------------------------------------------------------------------------
+      // Tokenizing
+      //-------------------------------------------------------------------------
+    public:
 
-    /// \brief Retrieves the next token in the series
-    ///
-    /// \param delim the delimiters to check
-    string_type next_token( const string_type& delim ) noexcept;
+      /// \brief Returns true if the tokenizer has more tokens available
+      ///
+      /// \return true if the tokenizer has more tokens
+      constexpr bool has_more() const noexcept;
 
-    /// Returns whether or not the supplied argument is a delimiter
-    ///
-    /// \param c value to check if delimiter
-    /// \param delim the delimiters to check
-    /// \return true if argument is a delimiter
-    bool is_delimiter( value_type c, const string_type& delim ) const noexcept;
+      /// \brief Gets the next token in this string tokenizer's string.
+      ///
+      /// \return the next token
+      string_type next() noexcept;
 
-    //-------------------------------------------------------------------------
-    // Private Members
-    //-------------------------------------------------------------------------
-  private:
+      /// \brief Gets the next token in this string tokenizer's string.
+      ///
+      /// First, the set of characters considered to be delimiters by this
+      /// basic_string_tokenizer object is changed to be the characters in
+      /// the string \p delim.
+      ///
+      /// Then the next token in the string after the current position is
+      /// returned. The current position is advanced beyond the recognized
+      /// token. The new delimiter set remains the default after this call
+      ///
+      /// \param delim the delimiter to match
+      /// \return the next token
+      string_type next( const string_type& delim ) noexcept;
 
-    string_type m_buffer;        /// The buffer to tokenize
-    string_type m_delimiter;     /// The delimiter used
-    size_type   m_current;       /// Current character being read
-    bool        m_return_delims; /// Are delimiters returned as tokens?
+      /// \brief Resets the current position within this string tokenizer
+      ///        to the beginning of the buffer
+      ///
+      /// This call allows for the basic_string_tokenizer to be re-used as if
+      /// reconstructed.
+      void reset() noexcept;
 
-  };
+      //-------------------------------------------------------------------------
+      // Private Member Functions
+      //-------------------------------------------------------------------------
+    private:
 
-  using string_tokenizer    = basic_string_tokenizer<char>;
-  using wstring_tokenizer   = basic_string_tokenizer<wchar_t>;
-  using u16string_tokenizer = basic_string_tokenizer<char16_t>;
-  using u32string_tokenizer = basic_string_tokenizer<char32_t>;
+      /// \brief Skips the delimiter, if \c return_delims was false
+      void skip_delimiter() noexcept;
 
+      /// \brief Retrieves the next token in the series
+      ///
+      /// \param delim the delimiters to check
+      string_type next_token( const string_type& delim ) noexcept;
+
+      /// Returns whether or not the supplied argument is a delimiter
+      ///
+      /// \param c value to check if delimiter
+      /// \param delim the delimiters to check
+      /// \return true if argument is a delimiter
+      bool is_delimiter( value_type c, const string_type& delim ) const noexcept;
+
+      //-------------------------------------------------------------------------
+      // Private Members
+      //-------------------------------------------------------------------------
+    private:
+
+      string_type m_buffer;        /// The buffer to tokenize
+      string_type m_delimiter;     /// The delimiter used
+      size_type   m_current;       /// Current character being read
+      bool        m_return_delims; /// Are delimiters returned as tokens?
+
+    };
+
+    using string_tokenizer    = basic_string_tokenizer<char>;
+    using wstring_tokenizer   = basic_string_tokenizer<wchar_t>;
+    using u16string_tokenizer = basic_string_tokenizer<char16_t>;
+    using u32string_tokenizer = basic_string_tokenizer<char32_t>;
+
+  } // namespace stl
 } // namespace bit
 
 #include "detail/string_tokenizer.inl"
