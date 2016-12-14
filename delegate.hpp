9d812cf4acd30a2456038385d9c18a07465d509d
Matthew Rodusek <matthew.rodusek@gmail.com>
2016-12-13 23:50:41 -0500

    Updated bit::delegate -> bit::stl::delegate

--- delegate.hpp
+++ delegate.hpp
@@ -20,125 +20,124 @@
 #include <utility>
 
 namespace bit {
+  namespace stl {
 
-  ////////////////////////////////////////////////////////////////////////////
-  /// \class bit::delegate
-  ///
-  /// \brief This class is for a lightweight way of managing function callbacks
-  ///        without requiring heap allocations.
-  ///
-  /// The syntax is a little off as a result, requiring a call to
-  /// #Delegate::bind.
-  ///
-  /// Example
-  /// \code
-  /// Delegate<void()> delegate;
-  /// T       t;
-  /// const T ct;
-  ///
-  /// delegate.bind<&foo>();           // bind free function
-  /// delegate.bind<T,&T::foo>( &t );  // bind member functions
-  /// delegate.bind<T,&T::foo>( &ct ); // bind const member function
-  /// \endcode
-  ///
-  /// \tparam Function signature and return type
-  ////////////////////////////////////////////////////////////////////////////
-  template<typename Function> class delegate;
-
-  template<typename R, typename...Args>
-  class delegate<R(Args...)> final
-  {
-    //-----------------------------------------------------------------------------
-    // Public Member Types
-    //-----------------------------------------------------------------------------
-  public:
-
-    using return_type        = R;
-    using free_function_type = return_type (*)(Args...);
-
-    template<class C>
-    using member_function_type = return_type (C::*)(Args...);
-
-    template<class C>
-    using const_member_function_type = return_type (C::*)(Args...) const;
-
-    //-----------------------------------------------------------------------------
-    // Constructor / Destructor
-    //-----------------------------------------------------------------------------
-  public:
-
-    /// \brief Constructs an unbound delegate
-    constexpr delegate() noexcept;
-
-    //-----------------------------------------------------------------------------
-    // Function Binding
-    //-----------------------------------------------------------------------------
-  public:
-
-    /// \brief Binds a free function to this delegate
+    ////////////////////////////////////////////////////////////////////////////
+    /// \brief This class is for a lightweight way of managing function callbacks
+    ///        without requiring heap allocations.
     ///
-    /// \tparam function the free-function type to bind
-    /// \tparam function the function pointer for the delegate
-    template<free_function_type function>
-    constexpr void bind() noexcept;
-
-    /// \brief Binds a member function to this delegate
+    /// The syntax is a little off as a result, requiring a call to
+    /// #Delegate::bind.
     ///
-    /// \tparam C The type of instance to bind
-    /// \tparam member_function The pointer to member function to bind
-    /// \param instance the instance to call the member function on
-    template <class C, member_function_type<C> member_function>
-    constexpr void bind( C* instance ) noexcept;
-
-    /// \brief Binds a const member function to this delegate
+    /// Example
+    /// \code
+    /// Delegate<void()> delegate;
+    /// T       t;
+    /// const T ct;
     ///
-    /// \tparam C the type of instance to bind
-    /// \tparam member_function The pointer to member function to bind
-    /// \param instance the instance to call the member function on
-    template <class C, const_member_function_type<C> member_function>
-    constexpr void bind( const C* instance ) noexcept;
-
-    //-----------------------------------------------------------------------------
-    // Queries
-    //-----------------------------------------------------------------------------
-  public:
-
-    /// \brief Is this Delegate bound?
+    /// delegate.bind<&foo>();           // bind free function
+    /// delegate.bind<T,&T::foo>( &t );  // bind member functions
+    /// delegate.bind<T,&T::foo>( &ct ); // bind const member function
+    /// \endcode
     ///
-    /// \return \c true if the delegate is bound
-    constexpr bool is_bound() const noexcept;
+    /// \tparam Function signature and return type
+    ////////////////////////////////////////////////////////////////////////////
+    template<typename Function> class delegate;
 
-    /// \brief Returns true if this delegate is bound
-    constexpr explicit operator bool() const noexcept;
+    template<typename R, typename...Args>
+    class delegate<R(Args...)> final
+    {
+      //-----------------------------------------------------------------------------
+      // Public Member Types
+      //-----------------------------------------------------------------------------
+    public:
 
-    //-----------------------------------------------------------------------------
-    // Invocation
-    //-----------------------------------------------------------------------------
-  public:
+      using return_type        = R;
+      using free_function_type = return_type (*)(Args...);
 
-    /// \brief Invokes the bound function
-    ///
-    /// \param args the arguments for the invokation
-    /// \return the return value for the invoked delegate
-    constexpr return_type invoke( Args&&...args ) const;
+      template<class C>
+      using member_function_type = return_type (C::*)(Args...);
 
-    //-----------------------------------------------------------------------------
-    // Private Member Types
-    //-----------------------------------------------------------------------------
-  private:
+      template<class C>
+      using const_member_function_type = return_type (C::*)(Args...) const;
 
-    using internal_function_type = return_type (*)(void*, Args...);
-    using stub_type              = std::pair<void*, internal_function_type>;
+      //-----------------------------------------------------------------------------
+      // Constructor / Destructor
+      //-----------------------------------------------------------------------------
+    public:
 
-    //-----------------------------------------------------------------------------
-    // Private Members
-    //-----------------------------------------------------------------------------
-  private:
+      /// \brief Constructs an unbound delegate
+      constexpr delegate() noexcept;
 
-    stub_type m_delegate_stub; ///< The internal stub for this delegate
+      //-----------------------------------------------------------------------------
+      // Function Binding
+      //-----------------------------------------------------------------------------
+    public:
 
-  };
+      /// \brief Binds a free function to this delegate
+      ///
+      /// \tparam function the free-function type to bind
+      /// \tparam function the function pointer for the delegate
+      template<free_function_type function>
+      constexpr void bind() noexcept;
 
+      /// \brief Binds a member function to this delegate
+      ///
+      /// \tparam C The type of instance to bind
+      /// \tparam member_function The pointer to member function to bind
+      /// \param instance the instance to call the member function on
+      template <class C, member_function_type<C> member_function>
+      constexpr void bind( C* instance ) noexcept;
+
+      /// \brief Binds a const member function to this delegate
+      ///
+      /// \tparam C the type of instance to bind
+      /// \tparam member_function The pointer to member function to bind
+      /// \param instance the instance to call the member function on
+      template <class C, const_member_function_type<C> member_function>
+      constexpr void bind( const C* instance ) noexcept;
+
+      //-----------------------------------------------------------------------------
+      // Queries
+      //-----------------------------------------------------------------------------
+    public:
+
+      /// \brief Is this Delegate bound?
+      ///
+      /// \return \c true if the delegate is bound
+      constexpr bool is_bound() const noexcept;
+
+      /// \brief Returns true if this delegate is bound
+      constexpr explicit operator bool() const noexcept;
+
+      //-----------------------------------------------------------------------------
+      // Invocation
+      //-----------------------------------------------------------------------------
+    public:
+
+      /// \brief Invokes the bound function
+      ///
+      /// \param args the arguments for the invokation
+      /// \return the return value for the invoked delegate
+      constexpr return_type invoke( Args&&...args ) const;
+
+      //-----------------------------------------------------------------------------
+      // Private Member Types
+      //-----------------------------------------------------------------------------
+    private:
+
+      using internal_function_type = return_type (*)(void*, Args...);
+      using stub_type              = std::pair<void*, internal_function_type>;
+
+      //-----------------------------------------------------------------------------
+      // Private Members
+      //-----------------------------------------------------------------------------
+    private:
+
+      stub_type m_delegate_stub; ///< The internal stub for this delegate
+
+    };
+  } // namespace stl
 }  // namespace bit
 
 #include "detail/delegate.inl"
--- detail/delegate.inl
+++ detail/delegate.inl
@@ -1,93 +1,95 @@
-#ifndef BIT_DETAIL_DELEGATE_INL
-#define BIT_DETAIL_DELEGATE_INL
+#ifndef BIT_STL_DETAIL_DELEGATE_INL
+#define BIT_STL_DETAIL_DELEGATE_INL
 
 namespace bit {
+  namespace stl {
 
-  //--------------------------------------------------------------------------
-  // Constructor
-  //--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+    // Constructor
+    //--------------------------------------------------------------------------
 
-  template<typename R, typename ... Args>
-  constexpr delegate<R(Args...)>::delegate() noexcept
-    :  m_delegate_stub(nullptr,nullptr)
-  {
-
-  }
-
-  //--------------------------------------------------------------------------
-  // Function Binding
-  //--------------------------------------------------------------------------
-
-  template<typename R, typename ... Args>
-  template<R (*function)(Args...)>
-  inline constexpr void delegate<R(Args...)>::bind()
-    noexcept
-  {
-    const auto callback = [](void*, Args...args )
+    template<typename R, typename ... Args>
+    constexpr delegate<R(Args...)>::delegate() noexcept
+      :  m_delegate_stub(nullptr,nullptr)
     {
-      return function(std::forward<Args>(args)...);
-    };
-    m_delegate_stub.first  = nullptr;
-    m_delegate_stub.second = +callback;
-  }
 
-  template<typename R, typename ... Args>
-  template<typename C, R (C::*member_function)(Args...)>
-  inline constexpr void delegate<R(Args...)>::bind( C* instance )
-    noexcept
-  {
-    const auto callback = [](void* ptr, Args...args )
+    }
+
+    //--------------------------------------------------------------------------
+    // Function Binding
+    //--------------------------------------------------------------------------
+
+    template<typename R, typename ... Args>
+    template<R (*function)(Args...)>
+    inline constexpr void delegate<R(Args...)>::bind()
+      noexcept
     {
-      return (static_cast<C*>(ptr)->*member_function)(std::forward<Args>(args)...);
-    };
-    m_delegate_stub.first  = instance;
-    m_delegate_stub.second = +callback;
-  }
+      const auto callback = [](void*, Args...args )
+      {
+        return function(std::forward<Args>(args)...);
+      };
+      m_delegate_stub.first  = nullptr;
+      m_delegate_stub.second = +callback;
+    }
 
-  template<typename R, typename ... Args>
-  template<typename C, R (C::*member_function)(Args...) const>
-  inline constexpr void delegate<R(Args...)>::bind( const C* instance )
-    noexcept
-  {
-     const auto callback = [](void* ptr, Args...args )
+    template<typename R, typename ... Args>
+    template<typename C, R (C::*member_function)(Args...)>
+    inline constexpr void delegate<R(Args...)>::bind( C* instance )
+      noexcept
     {
-      return (static_cast<const C*>(ptr)->*member_function)(std::forward<Args>(args)...);
-    };
-    m_delegate_stub.first  = const_cast<C*>(instance); // needed for const
-    m_delegate_stub.second = +callback;
-  }
+      const auto callback = [](void* ptr, Args...args )
+      {
+        return (static_cast<C*>(ptr)->*member_function)(std::forward<Args>(args)...);
+      };
+      m_delegate_stub.first  = instance;
+      m_delegate_stub.second = +callback;
+    }
 
-  //--------------------------------------------------------------------------
-  // Queries
-  //--------------------------------------------------------------------------
+    template<typename R, typename ... Args>
+    template<typename C, R (C::*member_function)(Args...) const>
+    inline constexpr void delegate<R(Args...)>::bind( const C* instance )
+      noexcept
+    {
+       const auto callback = [](void* ptr, Args...args )
+      {
+        return (static_cast<const C*>(ptr)->*member_function)(std::forward<Args>(args)...);
+      };
+      m_delegate_stub.first  = const_cast<C*>(instance); // needed for const
+      m_delegate_stub.second = +callback;
+    }
 
-  template<typename R, typename ... Args>
-  inline constexpr bool delegate<R(Args...)>::is_bound()
-    const noexcept
-  {
-    return m_delegate_stub.second != nullptr;
-  }
+    //--------------------------------------------------------------------------
+    // Queries
+    //--------------------------------------------------------------------------
 
-  template<typename R, typename...Args>
-  inline constexpr delegate<R(Args...)>::operator bool()
-    const noexcept
-  {
-    return m_delegate_stub.second != nullptr;
-  }
+    template<typename R, typename ... Args>
+    inline constexpr bool delegate<R(Args...)>::is_bound()
+      const noexcept
+    {
+      return m_delegate_stub.second != nullptr;
+    }
 
-  //--------------------------------------------------------------------------
-  // Invocation
-  //--------------------------------------------------------------------------
+    template<typename R, typename...Args>
+    inline constexpr delegate<R(Args...)>::operator bool()
+      const noexcept
+    {
+      return m_delegate_stub.second != nullptr;
+    }
 
-  template<typename R, typename ... Args>
-  inline constexpr typename delegate<R(Args...)>::return_type
-    delegate<R(Args...)>::invoke( Args&&... args )
-    const
-  {
-    BIT_ASSERT( m_delegate_stub.second != nullptr, "Cannot invoke unbound delegate");
-    return m_delegate_stub.second( m_delegate_stub.first, std::forward<Args>(args)... );
-  }
+    //--------------------------------------------------------------------------
+    // Invocation
+    //--------------------------------------------------------------------------
 
+    template<typename R, typename ... Args>
+    inline constexpr typename delegate<R(Args...)>::return_type
+      delegate<R(Args...)>::invoke( Args&&... args )
+      const
+    {
+      BIT_ASSERT( m_delegate_stub.second != nullptr, "Cannot invoke unbound delegate");
+      return m_delegate_stub.second( m_delegate_stub.first, std::forward<Args>(args)... );
+    }
+
+  } // namespace stl
 }  // namespace bit
 
-#endif /* BIT_DETAIL_DELEGATE_INL */
+#endif /* BIT_STL_DETAIL_DELEGATE_INL */
