3be4a461d9fdff50798a5bffd9cd85fe7e2af071
Matthew Rodusek <matthew.rodusek@gmail.com>
2017-04-08 01:35:56 -0500

    Added to_array utilities to array.hpp

--- include/bit/stl/array.hpp
+++ include/bit/stl/array.hpp
@@ -32,6 +32,7 @@ namespace bit {
       using array_return_type_t
         = std::array<array_return_type_helper_t<T, Types...>,sizeof...(Types)>;
 
+      struct to_array_impl;
     } // namespace details
 
     /// \brief Make utility for creating a std::array
@@ -46,6 +47,23 @@ namespace bit {
     template<typename T = detail::array_autodeduce, typename...Args>
     constexpr auto make_array(Args&&... args) noexcept
       -> detail::array_return_type_t<T, Args...>;
+
+    /// \brief Utility to convert raw arrays to std::array
+    ///
+    /// \param array a lvalue reference to an array
+    /// \return the std::array
+    template<typename T, std::size_t N>
+    constexpr std::array<T,N> to_array( const T(&array)[N] )
+      noexcept( std::is_nothrow_copy_constructible<T>::value );
+
+    /// \brief Utility to convert raw arrays to std::array
+    ///
+    /// \param array a rvalue reference to an array
+    /// \return the std::array
+    template<typename T, std::size_t N>
+    constexpr std::array<T,N> to_array( T(&&array)[N] )
+      noexcept( std::is_nothrow_move_constructible<T>::value );
+
 
   } // namespace stl
 } // namespace bit
