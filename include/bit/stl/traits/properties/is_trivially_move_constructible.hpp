96ebc76866fcfa7f6a86e58d9795cf61e79fbcf9
Matthew Rodusek <matthew.rodusek@gmail.com>
2017-11-16 20:23:10 -0500

    added is_trivially_move_constructible

---
+++ include/bit/stl/traits/properties/is_trivially_move_constructible.hpp
@@ -0,0 +1,45 @@
+/**
+ * \file is_trivially_move_constructible.hpp
+ *
+ * \brief This header defines a type trait for checking trivial
+ *        move-constructibilty
+ *
+ * \author Matthew Rodusek (matthew.rodusek@gmail.com)
+ */
+#ifndef BIT_STL_TRAITS_PROPERTIES_IS_TRIVIALLY_MOVE_CONSTRUCTIBLE_HPP
+#define BIT_STL_TRAITS_PROPERTIES_IS_TRIVIALLY_MOVE_CONSTRUCTIBLE_HPP
+
+#include "../composition/conjunction.hpp"
+
+#include <type_traits> // std::is_trivially_move_constructible
+
+namespace bit {
+  namespace stl {
+
+    /// \brief Type trait for determining if a type is move-constructible
+    ///
+    /// The result is aliased as \c ::value
+    template<typename T>
+    using is_trivially_move_constructible = std::is_trivially_move_constructible<T>;
+
+    /// \brief Helper utility to extract is_trivially_move_constructible::type
+    template<typename T>
+    constexpr bool is_trivially_move_constructible_v = is_trivially_move_constructible<T>::value;
+
+    //-------------------------------------------------------------------------
+
+    /// \brief Type trait for determining if a set of types are all
+    ///        move-constructible
+    ///
+    /// The result is aliased as \c ::value
+    template<typename...Ts>
+    struct are_trivially_move_constructible : conjunction<is_trivially_move_constructible<Ts>...>{};
+
+    /// \brief Helper utility to extract are_trivially_move_constructible::type
+    template<typename...Ts>
+    constexpr bool are_trivially_move_constructible_v = are_trivially_move_constructible<Ts...>::value;
+
+  } // namespace stl
+} // namespace bit
+
+#endif /* BIT_STL_TRAITS_PROPERTIES_IS_TRIVIALLY_MOVE_CONSTRUCTIBLE_HPP */
