87cb9cddf3a0cda6cc01accc51fe5b28bd5bd58d
Matthew Rodusek <matthew.rodusek@gmail.com>
2018-03-26 15:07:49 -0400

    Add functional subsystem
    This adds a subsystem for function objects. The following objects are
    added:
    - divides
    - minus
    - modulus
    - multiplies
    - negate
    - plus
    - bit_and
    - bit_not
    - bit_or
    - bit_xor
    - equal_to
    - equivalent_to
    - greater
    - greater_equal
    - less
    - less_equal
    - not_equal_to
    - use_first
    - use_nth_element
    - use_second
    - use_self
    - logical_and
    - logical_not
    - logical_or

---
+++ include/bit/stl/functional/arithmetic/multiplies.hpp
@@ -0,0 +1,62 @@
+/**
+ * \file multiplies.hpp
+ *
+ * \brief TODO: Add description
+ *
+ * \author Matthew Rodusek (matthew.rodusek@gmail.com)
+ */
+
+/*
+  The MIT License (MIT)
+
+  Bit Standard Template Library.
+  https://github.com/bitwizeshift/bit-stl
+
+  Copyright (c) 2018 Matthew Rodusek
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+#ifndef BIT_STL_FUNCTIONAL_ARITHMETIC_MULTIPLIES_HPP
+#define BIT_STL_FUNCTIONAL_ARITHMETIC_MULTIPLIES_HPP
+
+#include <utility> // std::forward
+
+namespace bit {
+  namespace stl {
+
+    /// \brief Function object for performing multiply operation.
+    ///        Effectively calls operator* on type T.
+    struct multiplies
+    {
+      template<typename T, typename U>
+      constexpr auto operator()( T&& lhs, U&& rhs ) const noexcept
+        -> decltype( std::forward<T>(lhs) * std::forward<U>(rhs) );
+    };
+  } // namespace stl
+} // namespace bit
+
+template<typename T, typename U>
+inline constexpr auto bit::stl::multiplies::operator()( T&& lhs, U&& rhs )
+  const noexcept
+  -> decltype( std::forward<T>(lhs) * std::forward<U>(rhs) )
+{
+  return std::forward<T>(lhs) * std::forward<U>(rhs);
+}
+
+#endif /* BIT_STL_FUNCTIONAL_ARITHMETIC_MULTIPLIES_HPP */
---
+++ include/bit/stl/functional/extraction/use_nth_element.hpp
@@ -0,0 +1,65 @@
+/**
+ * \file use_nth_tuple_element.hpp
+ *
+ * \brief TODO: Add description
+ *
+ * \author Matthew Rodusek (matthew.rodusek@gmail.com)
+ */
+
+/*
+  The MIT License (MIT)
+
+  Bit Standard Template Library.
+  https://github.com/bitwizeshift/bit-stl
+
+  Copyright (c) 2018 Matthew Rodusek
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+#ifndef BIT_STL_FUNCTIONAL_EXTRACTION_USE_NTH_ELEMENT_HPP
+#define BIT_STL_FUNCTIONAL_EXTRACTION_USE_NTH_ELEMENT_HPP
+
+#include <cstddef> // std::size_t
+#include <utility> // std::forward, std::get
+
+namespace bit {
+  namespace stl {
+
+    template<std::size_t N>
+    struct use_nth_element
+    {
+      template<typename Tuple>
+      constexpr decltype(auto) operator()( Tuple&& tuple );
+    };
+
+  } // namespace stl
+} // namespace bit
+
+template<std::size_t N>
+template<typename Tuple>
+inline constexpr decltype(auto) bit::stl::use_nth_element<N>
+  ::operator()( Tuple&& tuple )
+{
+  // ADL-lookup 'get<N>'
+  using std::get;
+
+  return get<N>( std::forward<Tuple>(tuple) );
+}
+
+#endif /* BIT_STL_FUNCTIONAL_EXTRACTION_USE_NTH_ELEMENT_HPP */
