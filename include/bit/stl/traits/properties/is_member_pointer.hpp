801cc8986affebb439fd5e0c1e1123967aa0ba3b
Matthew Rodusek <matthew.rodusek@gmail.com>
2018-03-26 12:34:59 -0400

    Add traits to detect member pointers
    The following traits were added:
    - is_member_function_pointer
    - is_member_object_pointer
    - is_member_pointer

---
+++ include/bit/stl/traits/properties/is_member_pointer.hpp
@@ -0,0 +1,38 @@
+/**
+ * \file is_member_pointer.hpp
+ *
+ * \brief TODO: Add description
+ *
+ * \author Matthew Rodusek (matthew.rodusek@gmail.com)
+ */
+#ifndef BIT_STL_TRAITS_PROPERTIES_IS_MEMBER_POINTER_HPP
+#define BIT_STL_TRAITS_PROPERTIES_IS_MEMBER_POINTER_HPP
+
+#include "../composition/disjunction.hpp"
+#include "../composition/conjunction.hpp"
+
+#include "is_member_object_pointer.hpp"
+#include "is_member_function_pointer.hpp"
+
+namespace bit {
+  namespace stl {
+
+    template<typename T>
+    struct is_member_pointer : disjunction<is_member_object_pointer<T>,
+                                           is_member_function_pointer<T>>{};
+
+    template<typename T>
+    constexpr bool is_member_pointer_v = is_member_pointer<T>::value;
+
+    //-------------------------------------------------------------------------
+
+    template<typename...Ts>
+    struct are_member_pointers : conjunction<is_member_pointer<Ts>...>{};
+
+    template<typename...Ts>
+    constexpr bool are_member_pointers_v = are_member_pointers<Ts...>::value;
+
+  } // namespace stl
+} // namespace bit
+
+#endif /* BIT_STL_TRAITS_PROPERTIES_IS_MEMBER_POINTER_HPP */
